3a9f3a2ad42cb6487fced8de98c1bafb5d32fbc7
Charles Raimbault <Charles@DESKTOP-HJ5IF0O.localdomain>
2019-01-23 21:35:39 +0100

    Export autre repo

--- includes/block.h
+++ includes/block.h
@@ -1,28 +1,29 @@
-#ifndef BLOCK_H
-#define BLOCK_H
+/*
+** EPITECH PROJECT, 2019
+** block.h
+** File description:
+** block
+*/
+
+#ifndef BLOCK_H_
+#define BLOCK_H_
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-// typedef struct s_block *block_t;
-// #define BLOCK_SIZE 20
-//
-#define align4(x) (((((x) -1) >>2) <<2)+8)// ATTENTION
-// struct s_block {
-// 	size_t size;
-// 	block_t next;
-// 	int free;
-// 	char data[1];
-// };
-
-void *malloc(size_t size);
-
+#include <string.h>
 
 typedef struct s_block block_t;
 
-struct s_block {
-    size_t size;
+struct s_block
+{
+    size_t  size;
     block_t *next;
-    // block_t *prev;
-    int free;
-    char data[1];
+    block_t *prev;
+    int     free;
+    void    *adresse;
 };
 
-#endif
+#endif /* !BLOCK_H_ */
--- srcs/main.c
+++ srcs/main.c
@@ -1,139 +1,178 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "block.h"
-#include <string.h>
+/*
+** EPITECH PROJECT, 2019
+** main.c
+** File description:
+** main.c
+*/
 
-block_t *find_block(block_t **last, size_t size);
+#include "block.h"
 
 block_t *base = NULL;
 
-void *extend_heap(block_t *last, size_t allign)
+void show_alloc_mem()
 {
-    block_t *new = sbrk(0);
+    block_t *tmp = base;
+    printf("break : %p\n", sbrk(0));
 
-    if (sbrk(sizeof(block_t) + allign) == (void *) - 1)
+    while (tmp) {
+        printf("%p - %p : %d bytes\n", tmp->adresse,
+        tmp->adresse + tmp->size, tmp->size);
+        tmp = tmp->next;
+    }
+}
+
+block_t * find_free_block(block_t *base, size_t const size)
+{
+    while (base && !(base->free && base->size >= size))
+        base = base->next;
+    if (base)
+        base->free = false;
+    return (base);
+}
+
+void *create_new_block(block_t *base, size_t const size)
+{
+    void *new_ptr = sbrk(sizeof(block_t) + size);
+
+    if (new_ptr == (void *) -1)
         return (NULL);
-    new->size = allign;
-    new->next = NULL;
-    // new->prev = last;
-    new->free = 0;
-    if (last)
-            last->next = new;
-    return (new);
-
+    while (base->next)
+        base = base->next;
+    base->next = new_ptr;
+    base->next->free = false;
+    base->next->size = size;
+    base->next->next = NULL;
+    base->next->prev = base;
+    base->next->adresse = sizeof(block_t) + new_ptr;
 }
 
-//void *calloc(size_t nmemb, size_t size)
-//{
-//    void *ptr = malloc(nmemb);
-//
-//    if (memset(ptr, 0, size) == NULL)
-//        return (NULL);
-//}
-
-void split_block(block_t *b, size_t s)
+block_t *start_mem(size_t const size)
 {
-    block_t *new;
+    block_t * new_mem;
+    void *new_ptr = sbrk(sizeof(block_t) + size);
 
-    new = b->data + s;
-    new->size = b->size - s - sizeof(block_t);
-    new->next = b->next;
-    // new->prev = b;
-    new->free = 1;
-    b->size = s;
-    b->next = new;
-    // if (new->next)
-    //     new->next->prev = new;
+    if (new_ptr == (void *) -1)
+        return (NULL);
+    new_mem = new_ptr;
+    new_mem->size = size;
+    new_mem->free = false;
+    new_mem->next = NULL;
+    new_mem->prev = NULL;
+    new_mem->adresse = sizeof(block_t) + new_ptr;
+    return (new_mem);
 }
 
-void *my_malloc(size_t size)
+void *malloc(size_t size)
 {
-    block_t *last;
-    block_t *b;
-    size_t s = align4(size);
+    block_t *new_block = NULL;
 
-    if (base) {
-        last = base;
-        b = find_block(&last, s);
-        if (b) {
-            if ((b->size -s ) >= (sizeof(block_t) + 8))
-                split_block(b, s);
-            b->free = 0;
-        }
-        else {
-            b = extend_heap(last, s);
-            if (b == NULL)
-                return (NULL);
-        }
-    }
-    else {
-        b = extend_heap(NULL, size);
-        if (b == NULL)
+    if (base == NULL) {
+        base = start_mem(size);
+        if (base == NULL)
             return (NULL);
-        base = b;
+        return (base->adresse);
     }
-    return (base->data);
+    new_block = find_free_block(base, size);
+    if (new_block == NULL)
+        new_block = create_new_block(base, size);
+    if (new_block == NULL)
+        return (NULL);
+    return (new_block->adresse);
+
 }
 
-block_t *find_block(block_t **last, size_t size)
+block_t* compare_ptr(void *ptr)
 {
     block_t *tmp = base;
 
-    while (tmp && !(tmp->free && tmp->size >= size)) {
-        *last = tmp;
+    if (tmp == NULL)
+        return (NULL);
+    while (tmp) {
+        if (tmp->adresse == ptr)
+            return (tmp);
         tmp = tmp->next;
     }
     return (tmp);
+}
 
+block_t *fusion(block_t *to_fusion)
+{
+    if (to_fusion->next == NULL)
+        return (to_fusion);
+    if (to_fusion->next->free == true) {
+        to_fusion->size = sizeof(block_t) + to_fusion->next->size;
+        to_fusion->next = to_fusion->next->next;
+        if (to_fusion->next)
+            to_fusion->next->prev = to_fusion;
+    }
+    return (to_fusion);
 }
 
 void free(void *ptr)
 {
-    // block_t *tmp = list;
-    //
-    // while (tmp) {
-    //     if ((tmp->adresse[0]) == (long long)ptr) {
-    //         break;
-    //     }
-    //     tmp = tmp->next;
-    // }
-    // if (tmp)
-    //     tmp->is_occuped = 0;
+    if (ptr == NULL) {
+        //INDIQUER UNE ERREUR
+        return;
+    }
+    block_t* to_free = compare_ptr(ptr);
+
+    if (to_free != NULL) {
+        to_free->free = true;
+        if (to_free->prev != NULL && to_free->prev->free == true)
+            to_free = fusion(to_free->prev);
+    }
 }
 
-void *realloc(void *ptr,size_t size)
+void *realloc(void *ptr, size_t size)
 {
+    block_t *old = NULL;
+    void    *new_ptr = NULL;
 
+    if (ptr == NULL)
+        return (NULL);
+    new_ptr = malloc(size);
+    if (new_ptr == NULL) {
+        free(ptr);
+        return (NULL);
+    }
+    // pas besoin d'un nouveau header
+    old = compare_ptr(ptr);
+    if (old == NULL)
+        return (NULL);
+    // copie de la memoire plutot que du header
+    // si tu veux je te fais un schema demain
+    new_ptr = memcpy(new_ptr, old->adresse, old->size);
+    free(ptr);
+    return (new_ptr);
 }
 
-// void show_mem_alloc()
+// void *calloc(int const nb, size_t const size)
 // {
-//     block_t *tmp = base;
+//     char *new = my_malloc(nb * size);
 //
-//     if (tmp) {
-//         printf("break : %p\n", tmp->data);
-//         tmp = tmp->next;
-//     }
-//     while (tmp != NULL) {
-//         printf("%d : %d bytes\n", , tmp->size);
-//         tmp = tmp->next;
-//     }
+//     if (new == NULL)
+//         return (NULL);
+//     for (size_t i = 0; i < size; ++i)
+//         new[i] = 0;
+//     return (new);
 // }
-//
+
 // int main()
 // {
+//     char *toto = my_calloc(2, sizeof(char) * 3);
 //
-//     int *p = my_malloc(sizeof(int) * 1);
-//    int *a = my_malloc(sizeof(int) * 240);
-//    int *c = my_malloc(sizeof(int) * 240);
+//     toto[0] = 'S';
+//     toto[1] = 'a';
+//     toto[2] = '\0';
 //
-//    for (int i = 0; i != 12; i++) {
-//        p[i] = 100 + i;
-//    }
-//    for (int i = 0; i != 12; i++) {
-//        printf("%d\n", p[i]);
-//    }
-//    // show_mem_alloc();
+//     printf("%s\n", toto);
+//     toto = my_realloc(toto, 4);
+//     toto[2] = 'b';
+//     toto[3] = '\0';
+//     printf("%s\n", toto);
+//     // test de free
+//     char *toto1 = my_malloc(sizeof(char) * 3);
+//
+//     char *toto2 = my_malloc(sizeof(char) * 5);
+//     show_alloc_mem();
 // }
